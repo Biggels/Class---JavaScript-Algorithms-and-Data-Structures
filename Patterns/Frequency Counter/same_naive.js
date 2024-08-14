35ed5d798bc6a3056b82edf83a2994456f6c38d8
Biggels <haackra@gmail.com>
2024-08-14 22:12:41 +0000

    implement dev container (and next lesson)

---
+++ Patterns/Frequency Counter/same_naive.js
@@ -0,0 +1,15 @@
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let squareIndex = arr2.indexOf(arr1[i] ** 2);
+        if (squareIndex === -1) {
+            return false;
+        }
+        arr2.splice(squareIndex, 1);
+    }
+    return true;
+}
+
+console.log(same([1, 2, 3], [1, 9, 4]))
---
+++ Patterns/Frequency Counter/same_refactor.js
@@ -0,0 +1,30 @@
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    const arr1Counts = {}
+    const arr2Counts = {}
+
+    for (let element of arr1) {
+        arr1Counts[element] = (arr1Counts[element] || 0) + 1;
+    }
+
+    for (let element of arr2) {
+        arr2Counts[element] = (arr2Counts[element] || 0) + 1;
+    }
+    console.log(arr1Counts);
+    console.log(arr2Counts);
+
+    for (let key in arr1Counts) {
+        if (!(key ** 2 in arr2Counts)) {
+            return false;
+        }
+        if (arr1Counts[key ** 2] !== arr2Counts[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(same([1, 2, 3], [1, 9, 4]))
